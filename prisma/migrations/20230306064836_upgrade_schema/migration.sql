6ac75088618fd4f09757cfc310c2edab969d9a26
Alexandre Sanchez <contact@alexandresanchez.xyz>
2023-03-06 07:54:30 +0100

    fix(db): broken migrations

--- prisma/migrations/20230306064836_upgrade_schema/migration.sql
+++ prisma/migrations/20230306064836_upgrade_schema/migration.sql
@@ -2,14 +2,16 @@
   Warnings:
 
   - A unique constraint covering the columns `[userStateId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `userStateId` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
 ALTER TABLE "UserState" DROP CONSTRAINT "UserState_userId_fkey";
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN     "userStateId" TEXT NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "userStateId" TEXT;
+
+-- Fill userStateId from userState
+UPDATE "User" SET "userStateId" = "userState"."id" FROM "UserState" AS "userState" WHERE "User"."id" = "userState"."userId";
 
 -- CreateTable
 CREATE TABLE "StreamKey" (
@@ -32,7 +34,7 @@ CREATE UNIQUE INDEX "StreamKey_userId_key" ON "StreamKey"("userId");
 CREATE UNIQUE INDEX "User_userStateId_key" ON "User"("userStateId");
 
 -- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_userStateId_fkey" FOREIGN KEY ("userStateId") REFERENCES "UserState"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "User" ADD CONSTRAINT "User_userStateId_fkey" FOREIGN KEY ("userStateId") REFERENCES "UserState"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "StreamKey" ADD CONSTRAINT "StreamKey_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
